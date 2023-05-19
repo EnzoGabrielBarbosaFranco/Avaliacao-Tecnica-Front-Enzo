279a66365b6c7a663f7f2809b8f45c403aee5e36
Enzo Gabriel <enzoddos7@gmail.com>
2023-05-19 14:59:28 -0400

    adição cabecalho e campo

---
+++ src/components/cabecalho.js
@@ -0,0 +1,21 @@
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+
+export default function Cabecalho() {
+    return (
+        <AppBar component="nav" style={{ backgroundColor: "#2C006A" }}>
+            <Toolbar>
+            <img src="./imagens/logo-digix.svg" alt="Logo" style={{ marginRight: "10px", width: "40px" }} />
+                <Typography
+                    variant="h4"
+                    fontFamily={'Train One'}
+                    sx={{ flexGrow: 1, padding: "30px", color: "white", border: "none", background: "none", cursor: "pointer" }}
+                >
+                    Desafio Digix
+                </Typography>
+            </Toolbar>
+        </AppBar >
+    );
+}
